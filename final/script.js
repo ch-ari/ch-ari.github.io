a09cf7a1e5a7f501c96229dde4b789fbc706b183
choeaeri <sorrytoyouch@gmail.com>
2024-06-03 12:31:54 +0900

    home.html과 관련 css 설정<br>help.html 구성

--- final/script.js
+++ final/script.js
@@ -1,7 +1,29 @@
 function open_help(url){
-    var left = (screen.availWidth-1000) / 2;
-    var top = (screen.availHeight-600) / 2 - 50;
-    var opt = "left=" + left + ",top=" + top + ",width=" + 1000  + ",height=" + 600;
+    var left = (screen.availWidth-1200) / 2;
+    var top = (screen.availHeight-800) / 2 - 50;
+    var opt = "left=" + left + ",top=" + top + ",width=" + 1200  + ",height=" + 800;
     const help_win=window.open(url, "pop", opt);
     if(help_win == null){ alert("팝업이 차단되어 있습니다!")}
+}
+
+function login(formname){
+    if(document.getElementById(formname).value != ""){
+        alert("로그인하기")
+    }else {
+        alert("입력된 것이 없습니다")
+    }
+    
+}
+
+function check_login(){
+    alert("로그인 하셨나요?")
+}
+
+function help_click(li){
+    for (var i = 0; i < document.querySelectorAll('.help_content').length; i++) {
+        document.querySelectorAll('.help_content')[i].style.display = 'none';
+        document.querySelectorAll('section > ul > li')[i].style.height='60px';
+    }
+    li.querySelector('.help_content').style.display='flex';
+    li.style.height='1000px';
 }
